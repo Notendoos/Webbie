5060492f769bf1625c36a521a9dc6acfd997bedd
Maikel Sleebos <mjsleebos@gmail.com>
2020-04-08 22:35:43 +0200

    Super deluxe cage feature

---
+++ dev/modules/commands/netflix.js
@@ -0,0 +1,5 @@
+function netflix (client,msg){
+    msg.channel.send("and chill?")
+}
+
+module.exports = netflix
--- dev/modules/commands/uncage.js
+++ dev/modules/commands/uncage.js
@@ -1,5 +1,5 @@
-function init(){
-
+function uncage(client,msg){
+    msg.channel.send(`http://www.placecage.com/${Math.round(Math.random()*50)+150}/${Math.round(Math.random()*50)+150}`)
 }
 
-module.exports = init
+module.exports = uncage
