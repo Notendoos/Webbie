7fc72d87527ce5cbce69fb6fe882e3cb20bdfc15
Maikel Sleebos <mjsleebos@gmail.com>
2020-04-08 21:17:59 +0200

    Added aliases

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 require('dotenv').config()
+require('module-alias/register')
 const discord = require('discord.js')
 const client = new discord.Client()
 const token = process.env.TOKEN
-const keepAlive = require('./dev/modules/util/keepAlive')
-const validate = require('./dev/modules/validation/validation')
+const keepAlive = require('@dev/util/keepAlive')
+const validate = require('@dev/validation/validation')
 
 client.on('ready',()=>{
     console.log('We\'re in bois')
-    console.log(client.user)
 })
 
 client.on('message', msg =>{
     if(msg.author.id != client.user.id){
-        console.log(validate.message(msg))
         validate.message(msg) && validate.command(msg)
     }
 })
