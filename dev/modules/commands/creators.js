7b1b2f9f2743bf5abd8b15e8ce48e2c4a2fb52d9
Maikel Sleebos <mjsleebos@gmail.com>
2020-04-09 13:02:04 +0200

    Added format, adjusted help

---
+++ dev/modules/commands/creators.js
@@ -0,0 +1,5 @@
+function init(client,msg){
+    msg.channel.send('```\nMade by Senpaizuri and Xiving (@Github)\nMIT License\n```')
+}
+
+module.exports = {init, desc:'These folks made me!'}
--- dev/modules/commands/format.js
+++ dev/modules/commands/format.js
@@ -1,6 +1,12 @@
 function init(client,msg){
-    const content = msg.content.split('webbie format')
-    msg.channel.send(`\`\`\`${lang}\n${code}\n\`\`\``)
+    const content = msg.content
+    const flavor = content.split('webbie format ')
+    if(flavor[0] == ''){
+        const code = flavor[1].split(' ')
+        msg.channel.send(`\`\`\`${code[0]}\n${code.slice(1,code.length).join(' ')}\n\`\`\``)
+    }else{
+        msg.channel.send('Give a flavor to format your code in!')
+    }   
 }
 
-module.exports = {init, desc:'I format code to markdown!'}
+module.exports = {init, desc:'I format code to markdown! I don\'t validate, That\'s on you!'}
